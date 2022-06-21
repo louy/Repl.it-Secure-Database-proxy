19ef27582311fcbb2e241c175c1a30aa69dced0c
Louay Alakkad <louay@alakkad.me>
2022-06-21 13:35:59 +0000

    API Key in URL

--- index.ts
+++ index.ts
@@ -1,46 +1,49 @@
-import {createServer, request} from 'http';
-import {request as requestHttps} from 'https';
-import {parse} from 'url';
+import { createServer, request, IncomingMessage, ServerResponse } from 'http';
+import { request as requestHttps } from 'https';
+import { parse } from 'url';
 
 createServer(handler).listen(80);
 
 const DB_URL = parse(process.env.REPLIT_DB_URL!);
 
 if (DB_URL.protocol !== 'http:' && DB_URL.protocol !== 'https:') {
-  throw new Error('Unknown DB URL protocol: ' +DB_URL.protocol);
+  throw new Error('Unknown DB URL protocol: ' + DB_URL.protocol);
 }
 
 const EXPECTED_API_KEY = process.env.API_KEY!
 if (!EXPECTED_API_KEY) {
   throw new Error('Missing API key. Please set API_KEY in environment.')
 }
+if (!EXPECTED_API_KEY.match(/^[A-Z0-9\-\_]{6,20}$/)) {
+  throw new Error('Invalid API key. It must be between 6 and 20 characters long, and can only contain letters, numbers, or one of the following: _-')
+}
 
-function handler(client_req, client_res) {
-  console.log('serve: ' + client_req.url);
+function handler(client_req: IncomingMessage, client_res: ServerResponse) {
+  console.log('serve: ' + client_req.method + ' ' + client_req.url);
 
-  // console.log(client_req.headers)
-
-  if (client_req.headers.get('x-api-key')!==EXPECTED_API_KEY) {
+  if (!client_req.url!.startsWith(EXPECTED_API_KEY + '/')) {
     client_res.writeHead(401)
-    client_res.send('Invalid api key')
+    client_res.write('Invalid api key\n')
     client_res.end();
     return;
   }
 
+  const url = client_req.url!.substring(EXPECTED_API_KEY.length+1);
+
   var options = {
     hostname: DB_URL.host,
     port: DB_URL.port,
-    path: DB_URL.pathname + client_req.url.replace(/\.+/g, '.'),
+    path: DB_URL.pathname + url.replace(/\.+/g, '.'),
     method: client_req.method,
     headers: {
-      'Content-Type': client_req.headers.get('content-type'),
+      'Content-Type': client_req.headers['content-type'],
     },
   };
- 
-  const reqFn = DB_URL.protocol === 'https:'?requestHttps:request;
+
+  const reqFn = DB_URL.protocol === 'https:' ? requestHttps : request;
 
   var proxy = reqFn(options, function (res) {
-    client_res.writeHead(res.statusCode, res.headers)
+    client_res.writeHead(res.statusCode!, res.headers)
     res.pipe(client_res, {
       end: true
     });
